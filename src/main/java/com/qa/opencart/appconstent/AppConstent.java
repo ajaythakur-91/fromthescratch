b36924901f0d6a7e94a5613a3824f74d2d736e6f
ajay <ajay.qa91@gmail.com>
2025-03-06 23:44:17 +0530

    add account page test

--- src/main/java/com/qa/opencart/appconstent/AppConstent.java
+++ src/main/java/com/qa/opencart/appconstent/AppConstent.java
@@ -1,11 +1,15 @@
 package com.qa.opencart.appconstent;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AppConstent {
 
     public static final String QA_ENV_PATH = "src/test/resources/config/Qa_config.properties";
     public static final String LOGIN_PAGE_TITLE = "Account Login";
     public static final String ACCOUNT_PAGE_TITLE ="My Account";
     public static final String LOGIN_PAGE_URL_FRACTION = "account/login";
-
+    public static final String ACCOUNT_PAGE_URL_FRACTION = "account/account";
+    public static final List<String>ACCOUNT_PAGE_HEADER = Arrays.asList("My Account","My Orders","My Affiliate Account","Newsletter");
 
 }
--- src/main/java/com/qa/opencart/apperr/AppErr.java
+++ src/main/java/com/qa/opencart/apperr/AppErr.java
@@ -5,5 +5,6 @@ public class AppErr {
     public static final String TITLE_NOT_MATCHED = "TITLE NOT MATCHED";
     public static final String LINK_NOT_MATCHED = "LINK NOT MATCHED";
     public static final String COUNT_NOT_MATCHED = "COUNT NOT MATCHED";
+    public static final String LIST_NOT_MATCHED = "LIST NOT MACHED";
     // public static final String
 }
